175e96f2d051771146fa540909455efbae5556d9
ImproperIssues <ItziSpyder@gmail.com>
2023-04-10 16:25:40 -0700

    updated code again

---
+++ src/main/java/io/github/itzispyder/clickcrystals/modules/modules/FullBright.java
@@ -0,0 +1,24 @@
+package io.github.itzispyder.clickcrystals.modules.modules;
+
+import io.github.itzispyder.clickcrystals.modules.Categories;
+import io.github.itzispyder.clickcrystals.modules.Module;
+
+/**
+ * FullBright module
+ */
+public class FullBright extends Module {
+
+    public FullBright() {
+        super("FullBright", Categories.OPTIMIZATION,"Gives you infinite night vision.");
+    }
+
+    @Override
+    protected void onEnable() {
+
+    }
+
+    @Override
+    protected void onDisable() {
+
+    }
+}
---
+++ src/main/java/io/github/itzispyder/clickcrystals/modules/modules/SlowHandSwing.java
@@ -0,0 +1,24 @@
+package io.github.itzispyder.clickcrystals.modules.modules;
+
+import io.github.itzispyder.clickcrystals.modules.Categories;
+import io.github.itzispyder.clickcrystals.modules.Module;
+
+/**
+ * Slow hand swing module
+ */
+public class SlowHandSwing extends Module {
+
+    public SlowHandSwing() {
+        super("SlowHandSwing", Categories.MISC,"Swings your hand slowly like you have mining fatigue. Perfect for crystal pvp!");
+    }
+
+    @Override
+    protected void onEnable() {
+
+    }
+
+    @Override
+    protected void onDisable() {
+
+    }
+}
